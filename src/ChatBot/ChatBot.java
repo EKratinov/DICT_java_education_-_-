88078cacf1f4afe1336b0aae7fd618da06ecc1a8
EKratinov <y.a.kratynov@student.itc.khai.edu>
2024-10-06 12:28:50 +0300

    ChatBot 1st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,11 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        String bot_name = "ChatBot";
+        int birth_year = 2024;
+
+        System.out.println("Hello! My name is " + bot_name + ".");
+        System.out.println("I was created in " + birth_year + ".");
+    }
+}
