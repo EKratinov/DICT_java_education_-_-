96099a9be7f4158abb666733bded89e63cd5b969
EKratinov <y.a.kratynov@student.itc.khai.edu>
2024-10-13 14:49:52 +0300

    Hangman 5-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -7,18 +7,35 @@ public class hangman {
         Scanner scanner = new Scanner(System.in);
         String[] words = {"python", "java", "javascript", "kotlin"};
         String chosenWord = words[new Random().nextInt(words.length)];
-        String hint = chosenWord.substring(0, 2) + "-".repeat(chosenWord.length() - 2);
+        StringBuilder displayWord = new StringBuilder("-".repeat(chosenWord.length()));
+        int attempts = 8;
 
         System.out.println("HANGMAN");
-        System.out.println("Guess the word " + hint + ": > ");
-        String guess = scanner.nextLine();
 
-        if (guess.equals(chosenWord)) {
+        while (attempts > 0 && displayWord.toString().contains("-")) {
+            System.out.println("Current word: " + displayWord);
+            System.out.println("Input a letter: > ");
+            char letter = scanner.nextLine().charAt(0);
+
+            if (chosenWord.indexOf(letter) >= 0) {
+                for (int i = 0; i < chosenWord.length(); i++) {
+                    if (chosenWord.charAt(i) == letter) {
+                        displayWord.setCharAt(i, letter);
+                    }
+                }
+            } else {
+                System.out.println("That letter doesn't appear in the word.");
+                attempts--;
+            }
+        }
+
+        if (displayWord.toString().equals(chosenWord)) {
             System.out.println("You survived!");
         } else {
-            System.out.println("You lost!");
+            System.out.println("You lost! The word was " + chosenWord + ".");
         }
 
         scanner.close();
     }
 }
+
