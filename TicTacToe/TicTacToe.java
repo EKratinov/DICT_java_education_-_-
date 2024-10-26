fdebee56fd96e47cc1d2c0c93f38195ceb6dc54b
EKratinov <y.a.kratynov@student.itc.khai.edu>
2024-10-26 14:18:07 +0300

    TicTacToe 4-th stage

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -3,24 +3,18 @@ import java.util.Scanner;
 public class TicTacToe {
     private static char[][] board = new char[3][3];
 
+
     public static void main(String[] args) {
-        // Этап 1: Инициализация пустого поля и печать начального состояния
         initializeBoard();
         printBoard();
 
-        // Этап 2: Ввод состояния поля и печать его в виде сетки 3x3
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter cells (use 'X', 'O', or '_'): ");
-        String input = scanner.nextLine();
-        updateBoard(input);
-        printBoard();
-
-        // Этап 3: Анализ состояния поля и вывод результата
-        String result = analyzeBoard();
-        System.out.println(result);
+        while (true) {
+            playerMove('X');
+            printBoard();
+        }
     }
 
-    // Инициализация пустого поля
+
     public static void initializeBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -29,14 +23,14 @@ public class TicTacToe {
         }
     }
 
-    // Обновление игрового поля на основе ввода пользователя
+
     public static void updateBoard(String input) {
         for (int i = 0; i < 9; i++) {
             board[i / 3][i % 3] = input.charAt(i) == '_' ? ' ' : input.charAt(i);
         }
     }
 
-    // Печать текущего состояния игрового поля
+
     public static void printBoard() {
         System.out.println("---------");
         for (char[] row : board) {
@@ -49,44 +43,28 @@ public class TicTacToe {
         System.out.println("---------");
     }
 
-    // Анализ состояния игры
-    public static String analyzeBoard() {
-        boolean xWins = checkWin('X');
-        boolean oWins = checkWin('O');
-        int xCount = 0, oCount = 0, emptyCount = 0;
 
-        // Подсчёт символов X, O и пустых ячеек
-        for (char[] row : board) {
-            for (char cell : row) {
-                if (cell == 'X') xCount++;
-                else if (cell == 'O') oCount++;
-                else emptyCount++;
+    public static void playerMove(char symbol) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter the coordinates: ");
+            String[] coordinates = scanner.nextLine().split(" ");
+
+            try {
+                int row = Integer.parseInt(coordinates[0]) - 1;
+                int col = Integer.parseInt(coordinates[1]) - 1;
+
+                if (row < 0 || row >= 3 || col < 0 || col >= 3) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                } else if (board[row][col] != ' ') {
+                    System.out.println("This cell is occupied! Choose another one!");
+                } else {
+                    board[row][col] = symbol;
+                    break;
+                }
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("You should enter numbers!");
             }
         }
-
-        // Проверка различных состояний игры
-        if (xWins && oWins || Math.abs(xCount - oCount) > 1) {
-            return "Impossible";
-        } else if (xWins) {
-            return "X wins";
-        } else if (oWins) {
-            return "O wins";
-        } else if (emptyCount == 0) {
-            return "Draw";
-        } else {
-            return "Game not finished";
-        }
-    }
-
-    // Проверка выигрыша для указанного символа
-    public static boolean checkWin(char symbol) {
-        for (int i = 0; i < 3; i++) {
-            if ((board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) ||  // Проверка строк
-                    (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol)) {  // Проверка столбцов
-                return true;
-            }
-        }
-        return (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) ||  // Диагональ слева направо
-                (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol);    // Диагональ справа налево
     }
 }
