89d5b4fc850719787fc7e31989015abe3c757f84
EKratinov <y.a.kratynov@student.itc.khai.edu>
2024-10-26 13:54:20 +0300

    TicTacToe 1-st stage

---
+++ TicTacToe/TicTacToe.java
@@ -0,0 +1,23 @@
+public class TicTacToe {
+    private static char[][] board = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+    };
+
+    public static void printBoard() {
+        System.out.println("---------");
+        for (char[] row : board) {
+            System.out.print("| ");
+            for (char cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+
+    public static void main(String[] args) {
+        printBoard();
+    }
+}
